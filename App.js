9e554626b50130a5e90ea5db9f208bc3bdad998e
fridayteam23 <fridaycompany23@gmail.com>
2019-06-20 22:30:45 +0300

    Button degisilik

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Button, Text, View} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,12 +18,19 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  fonk = () => {
+    alert ('Merhaba Deneme')
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Button 
+         title ="Onur"
+         onPress={this.fonk}
+        />
       </View>
     );
   }
